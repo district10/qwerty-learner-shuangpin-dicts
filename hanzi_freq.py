74aaca12b5262b3fe434c9a00896e6113af56371
TANG ZHIXIONG <zhixiong@momenta.ai>
2021-02-16 18:18:36 +0800

    frequence of zhihu text

--- hanzi_freq.py
+++ hanzi_freq.py
@@ -6,6 +6,11 @@ import argparse
 import json
 import re
 from functools import cmp_to_key
+from tqdm import tqdm
+import tempfile
+from main import hanzi2keys
+import numpy as np
+import random
 
 
 def count_words(line: str, *, counter: Dict[str, int]):
@@ -18,8 +23,6 @@ def count_words(line: str, *, counter: Dict[str, int]):
 
 
 if __name__ == '__main__':
-    # sys.argv = 'hanzi_freq.py --input source/sample2.txt'.split()
-
     prog = 'hanzi frequency counter'
     description = ('count hanzi words frequency')
     parser = argparse.ArgumentParser(prog=prog, description=description)
@@ -41,15 +44,23 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
     input_paths: List[str] = args.input
-    output_dict_path: Optional[str] = args.output_dict
-    output_freq_path: Optional[str] = args.output_frequency
+    output_dict_path: str = args.output_dict or tempfile.mktemp(suffix='.json')
+    output_freq_path: str = args.output_frequency or tempfile.mktemp(suffix='.json')
     args = None
 
     counter = defaultdict(int)
     for path in input_paths:
         with open(path) as f:
-            for line in f.readlines():
-                count_words(line, counter=counter)
+            if path.endswith('.json'):
+                print(f'loading frequence file from {path}...')
+                for word, count in json.load(f).items():
+                    if isinstance(count, int) and count > 0:
+                        counter[word] += count
+            else:
+                print(f'loading text file from {path}...')
+                lines = f.readlines()
+                for line in tqdm(lines, f'counting words of #{len(lines):,} lines...'):
+                    count_words(line, counter=counter)
     for key in list(counter.keys()):
         if not len(re.findall(r'[\u4e00-\u9fff]', key)):
             counter.pop(key, None)
@@ -58,16 +69,31 @@ if __name__ == '__main__':
     counter = OrderedDict()
     for freq, word in freq2word:
         counter[word] = freq
-    pprint(counter)
+    # pprint(counter)
 
-    if output_freq_path:
-        output_freq_path = os.path.abspath(output_freq_path)
-        os.makedirs(os.path.dirname(output_freq_path), exist_ok=True)
-        with open(output_freq_path, 'w') as f:
-            json.dump(counter, f, indent=4, ensure_ascii=False)
-        print(f'wrote to {output_freq_path}')
+    if not output_freq_path:
+        output_freq_path = tempfile.mktemp(suffix='.json')
+    output_freq_path = os.path.abspath(output_freq_path)
+    os.makedirs(os.path.dirname(output_freq_path), exist_ok=True)
+    with open(output_freq_path, 'w') as f:
+        json.dump(counter, f, indent=4, ensure_ascii=False)
+    print(f'wrote to {output_freq_path}')
 
-    if output_dict_path:
-        output_dict_path = os.path.abspath(output_dict_path)
-        os.makedirs(os.path.dirname(output_dict_path), exist_ok=True)
-        pass
+    output_dict_path = os.path.abspath(output_dict_path)
+    os.makedirs(os.path.dirname(output_dict_path), exist_ok=True)
+    with open(output_dict_path, 'w') as f:
+        upper_thresh = list([i[0] for i in freq2word])[10] # select a big freq
+        items = [{
+            'name': ''.join(hanzi2keys(w, shuangpin_schema='ziranma')),
+            'trans': [w, c, int(np.log(min(c, upper_thresh))) + 1],
+        } for w, c in counter.items()]
+        ret = []
+        for i in items:
+            ret.extend([i] * i['trans'][-1])
+        for k in list(range(len(ret) // 50)):
+            i, j = k * 50, (k+1) * 50
+            copy = list(ret[i:j])
+            random.shuffle(copy)
+            ret[i:j] = copy
+        json.dump(ret, f, indent=4, ensure_ascii=False)
+    print(f'wrote to {output_dict_path}')
